e7f299519968acf5aab6b13f3a9cdfb52abd2276
Zhanghaochen <zhanghaochen@perfxlab.com>
2026-03-02 17:12:57 +0800

    first add caxpy,daxpy,saxpy,zaxpy

---
+++ src/flag_blas/__init__.py
@@ -0,0 +1,9 @@
+"""
+flag_blas - BLAS operations implemented with Triton
+"""
+
+from . import ops
+
+__version__ = "0.1.0"
+
+__all__ = ["ops"]
---
+++ src/flag_blas/ops/__init__.py
@@ -0,0 +1,7 @@
+"""
+BLAS Level 1 operations
+"""
+
+from .axpy import saxpy, daxpy, caxpy, zaxpy
+
+__all__ = ["saxpy", "daxpy", "caxpy", "zaxpy"]
