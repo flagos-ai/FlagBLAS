3777bad0ea52e44a426349f1e4f115f9f761616e
bzhangperfxlab <zhangbin@perfxlab.com>
2026-03-07 20:09:37 +0800

    add KERNEL_ID in libtuner for demo_op

--- src/flag_blas/ops/__init__.py
+++ src/flag_blas/ops/__init__.py
@@ -3,5 +3,13 @@ BLAS Level 1 operations
 """
 
 from flag_blas.ops.level1.axpy import saxpy, daxpy, caxpy, zaxpy
+from flag_blas.ops.demo_op import sscal, dscal
 
-__all__ = ["saxpy", "daxpy", "caxpy", "zaxpy"]
+__all__ = [
+        "saxpy", 
+        "daxpy", 
+        "caxpy", 
+        "zaxpy",
+        "sscal", 
+        "dscal",
+    ]
